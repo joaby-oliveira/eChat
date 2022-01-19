e441cf27b04fda686079bd02cb80a80336e8c6cf
joaby <joabyoliveira2004@gmail.com>
2022-01-18 22:31:21 -0300

    fix: ts error

--- src/Components/Title/index.tsx
+++ src/Components/Title/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./styles.module.scss";
 
-export const Title = (props) => {
+interface Props {
+  props: any;
+  children: any;
+}
+
+export const Title: React.FC<{}> = ({ children }) => {
   return (
-    <h1 className={`${styles.Title} flex column crossCenter`}>
-      {props.children}
-    </h1>
+    <h1 className={`${styles.Title} flex column crossCenter`}>{children}</h1>
   );
 };
